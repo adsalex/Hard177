ff2c30e055001b968baea922d1eca8c3923deefe
adsalex <adsalex77@gmail.com>
2023-03-16 13:22:13 +0300

    updated site

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
+var menutrig=false
 var anc_color=null;
 const new_anc_color="rgb(255, 165, 0)"
 var mynav =document.getElementsByTagName("nav")
+var menu_button=document.getElementById("menu_button")
+var content_elem=document.getElementById("content")
+menu_button.addEventListener("click",open_close)
 for(let navig_sel of mynav)
 {
 navig_sel.addEventListener("mouseover",nav_down)
@@ -17,3 +21,17 @@ function nav_up(handler)
    
     handler.target.style.background=anc_color
 }
+function open_close()
+{
+    
+    if(menutrig)
+    {
+        mynav[0].style.display="none"
+    }
+    else
+    {
+        mynav[0].style.display="flex"
+    }
+    menutrig=!menutrig
+}
+
