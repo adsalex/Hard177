c9d07953d02a6cfb6bdfa154a68ab8afc7d9763b
adsalex <adsalex77@gmail.com>
2023-04-05 12:14:55 +0300

    frontend ready

--- scripts/main.js
+++ scripts/main.js
@@ -81,7 +81,7 @@ for(let elem=0; elem< rootxml.childElementCount;elem++)
     +"</a> </figcaption>"
     +"</figure>")
 }
-console.log(rootxml)
+
 document.getElementsByClassName("content")[0].innerHTML=content
 }}
 }
@@ -108,18 +108,25 @@ let rootxml=xmlcode.getElementsByTagName("catalog")[0]
 //console.log(rootxml)
 for(let elem=0; elem< rootxml.childElementCount;elem++)
 {
-    //if(rootxml.childNodes[0].tag)
+    let articul_buffer=rootxml.getElementsByTagName("articul")[elem].innerHTML
+    console.log(rootxml.getElementsByTagName("articul")[elem].innerHTML)
     content+=("<article class='goodbar'>"
     +"<img alt='photo not found' src='"+rootxml.getElementsByTagName("photo")[elem].innerHTML  +"'/>"
     +"<p>"+rootxml.getElementsByTagName("goodname")[elem].innerHTML+"</p>"
     +"<p>"+"$" +rootxml.getElementsByTagName("price")[elem].innerHTML+"</p>"
     +"<p>"+rootxml.getElementsByTagName("articul")[elem].innerHTML+"</p>"
     +"<div>"+rootxml.getElementsByTagName("description")[elem].innerHTML+"</div>"
-    +"<button value="+elem+" class='addbutton' onClick='addtocart("+elem+")' "+">добавить в корзину </button>"
+    +"<button value="+articul_buffer+" class='addbutton' "+">добавить в корзину </button>"
     +"</article>")
 }
-
+console.log(rootxml.getElementsByTagName("articul")[0].innerHTML)
 document.getElementsByClassName("content")[0].innerHTML=content
+
+for(let button of document.getElementsByClassName("addbutton"))
+{
+    button.addEventListener("click",addtocart)
+}
+
 }}
 
 if(window.localStorage.getItem("cart_items")==null)
@@ -127,16 +134,21 @@ if(window.localStorage.getItem("cart_items")==null)
 window.localStorage.setItem("cart_items",[])
 }
 
-let buttongeter=( document.getElementsByTagName("button"))
-let butt_array=[...buttongeter]
 
-function addtocart(elem)
+
+function addtocart(handler)
 {
+
+let elem=0
+
+
 
 //alert("helll")
 let buff={}
 if((window.localStorage.getItem("cart_items"))){buff = JSON.parse(window.localStorage.getItem("cart_items"))}
-console.log(buff)
+
+for(let prop in buff ){if(buff[prop].articul==handler.target.value){break};elem++}
+
 let buff_obj=
 {   
     goodname:catfile.getElementsByTagName("goodname")[elem].innerHTML,
@@ -176,16 +188,22 @@ if(page_name=="cart.html")
             +order_list[elem].photo+"'/> "+"<div class='texthold'>"+"<p>"+order_list[elem].goodname+"</p>"
             +"<p>"+order_list[elem].articul+"</p>"+"<p>$"+order_list[elem].price+"</p>"
             +"<div>"+order_list[elem].description+"</div>"
-            +"</div>"+"<input type='text' class='count' onchange='update_count("+order_list[elem].count+","+elem+")' value="+order_list[elem].count+" width=6em/>" 
+            +"</div>"+"<input type='text' class='count' value="+order_list[elem].count+" width=6em/>" 
             +"<button OnClick=delete_item("+elem+")> удалить </button>"
             +"</article>"
             totprice+=Number(order_list[elem].price)*order_list[elem].count
         }
-        
+
         content_buff+="<section> <button id='order'> оформить заказ </button>"
-        +"<p> Итоговая цена "+totprice+"</p>"
+        +"<p> Итоговая цена $<span id='price_show'>"+totprice+"</span></p>"
         +"<p> Телефон "+"<input type='text' id='phone'/>"+"</p> </section>"
         document.getElementsByClassName("content")[0].innerHTML=content_buff
+        count_holders = document.getElementsByClassName("count")
+        let index_counter=0
+        
+        //for(let prop in buff ){if(buff[prop].articul==handler.target.value){break};elem++}
+        for(let elem of count_holders){const buffer=index_counter;elem.addEventListener("change",(handler)=>{update_count(handler,buffer)});index_counter++;}
+        
     }
 
     updater()
@@ -198,7 +216,7 @@ if(page_name=="cart.html")
         request.setRequestHeader('Content-Type', 'application/json')
         request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 
-        request.send("{'hello':66}")
+        request.send(JSON.stringify(window.localStorage.getItem("cart_items")))
     }
     function delete_item(elem)
     {
@@ -208,14 +226,32 @@ if(page_name=="cart.html")
         updater()
         
     }
-    function update_count(value,pos)
+    function update_count(handler,pos)
     {
-        let buffer_mod=JSON.parse(window.localStorage.getItem("cart_items")) 
+
+        let index =0
+        let articul
         
-        buffer_mod[pos].count=value
+       
+        let buffer_mod=JSON.parse(window.localStorage.getItem("cart_items")) 
+
+        /*for(let prop in buffer_mod ){if(index==pos){articul=prop;break};index++}
+
+        console.log(articul,pos)//*/
+        console.log(pos)
+        buffer_mod[pos].count=handler.target.value
+        
+
+        //price count ends
         window.localStorage.setItem("cart_items",JSON.stringify(buffer_mod))
         console.log(buffer_mod)
-        //updater()
+        let total_price_changed=0
+        let order_list=JSON.parse(window.localStorage.getItem("cart_items"))
+        for(let price_elem in order_list)
+        {
+            total_price_changed+=(Number(order_list[price_elem].price*order_list[price_elem].count))
+        }
+        document.getElementById("price_show").innerHTML=total_price_changed
     }
 }
 ////cart end
